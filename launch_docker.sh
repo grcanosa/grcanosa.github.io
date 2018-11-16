a61ca19abfa77f1e9dde4ee8d8947f6ad9fad978
grcanosa <grcanosa@gmail.com>
2018-11-16 17:44:48 +0100

    Removed everything from old template

--- launch_docker.sh
+++
@@ -1,5 +0,0 @@
-docker container rm grcanosa_blog
-docker container rm grcanosa_blog
-#docker image rm grcanosa_blog
-docker build -t grcanosa_blog .
-docker run -p 4000:4000 -v $PWD:/srv/jekyll grcanosa_blog
