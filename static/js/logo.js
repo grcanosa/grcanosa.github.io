66b86864767f2ac7c7fe01ce5b60b6f1054bc0af
grcanosa <grcanosa@gmail.com>
2018-11-16 17:43:53 +0100

    Added to old_template

--- static/js/logo.js
+++ static/js/logo.js
@@ -9,12 +9,12 @@ SVG.on(document, 'DOMContentLoaded', function() {
     var rect = draw.rect(l1+l2, h).fill("none").stroke(color1)
     //var rect2 = draw.rect(l1, h).fill(color1).stroke(color1)
     var initial = draw.plain("g").fill("white")
-            .cx(l1/2).cy(h/2).size(h)
+            .x(0).y(0).size(h)
             .attr({
               "text-anchor":"middle"
               , "dominant-baseline":"central"
               , "font-family": "'Open Sans', sans-serif"
-              , "font-weight": 700
+              , "font-weight": 600
             })
     var bbox = initial.bbox()
     
@@ -23,7 +23,7 @@ SVG.on(document, 'DOMContentLoaded', function() {
                             "text-anchor":"start"
                             , "dominant-baseline":"central"
                             , "font-family": "'Open Sans', sans-serif"
-                            , "font-weight": 700
+                            , "font-weight": 600
                           })
-    var rect2 = draw.rect(bbox.w, bbox.h).fill(color1).stroke(color1).x(bbox.x).y(bbox.y).back()
+    // rect2 = draw.rect(bbox.w, bbox.h).fill(color1).stroke(color1).x(bbox.x).y(bbox.y).back()
   })
