56230823366d5a5b195d2f1933bffa89b69700c9
grcanosa <grcanosa@gmail.com>
2018-11-18 16:31:09 +0100

    Added correct README.md

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker container rm grcanosa_blog2
+docker container rm grcanosa_blog
 
 #docker image rm grcanosa_blog
-docker build -t grcanosa_blog2 .
-docker run --rm --name grcanosa_blog2 -p 4001:4000 -v $PWD:/srv/jekyll grcanosa_blog2
+docker build -t grcanosa_blog .
+docker run --rm --name grcanosa_blog -p 4001:4000 -v $PWD:/srv/jekyll grcanosa_blog
